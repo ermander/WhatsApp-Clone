fb507b7fc31f62e4c573252acc53e5aaf021d624
Emanuele Bertuol <emanuelebertuol@gmail.com>
2021-04-28 08:50:59 +0200

    .

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -15,7 +15,7 @@ function Chat() {
   const { roomId } = useParams();
   const [roomName, setRoomName] = useState("");
   const [messages, setMessages] = useState([]);
-  const [{ user }, dispatch] = useStateValue()
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
     console.log(roomId);
@@ -56,7 +56,15 @@ function Chat() {
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
           <h3>{roomName}</h3>
-          <p>Last seen at...</p>
+          <p>
+            last seen{" "}
+            {
+              new Date(
+                messages[messages.length -1]?. 
+                timestamp?.toDate()
+              ).toUTCString()
+            }
+          </p>
         </div>
         <div className="chat_headerRight">
           <IconButton>
@@ -72,9 +80,11 @@ function Chat() {
       </div>
       <div className="chat__body">
         {messages.map((message) => (
-          <p className={`chat__message ${
-            message.name === user.displayName && "chat__reciever"
-            }`}>
+          <p
+            className={`chat__message ${
+              message.name === user.displayName && "chat__reciever"
+            }`}
+          >
             <span className="chat__name">{message.name}</span>
             {message.message}
             <span className="chat__timeStamp">
