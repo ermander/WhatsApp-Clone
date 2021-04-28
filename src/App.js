9aa3f0cd74fb33dc70e7f715ae77b5f0c406b7f1
Emanuele Bertuol <emanuelebertuol@gmail.com>
2021-04-28 07:28:22 +0200

    .

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Login from "./Login";
+import React, { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Switch>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
             <Sidebar />
-
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+            <Switch>
+              <Route path="/room/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
